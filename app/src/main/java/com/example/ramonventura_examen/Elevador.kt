e6446d6393c3de3abb46d840e04cd567735183a3
Ramón Ventura <50932514+Ramon-Ventura@users.noreply.github.com>
2019-10-23 18:35:00 -0600

    Mas cambios

--- app/src/main/java/com/example/ramonventura_examen/Elevador.kt
+++ app/src/main/java/com/example/ramonventura_examen/Elevador.kt
@@ -5,4 +5,22 @@ enum class EstadoElevador(){
 }
 class Elevador (var piso: Int, var estado: EstadoElevador) {
 
+    fun Subir(piso_usuario:Int){
+        estado = EstadoElevador.ARRIBA
+        while(piso<piso_usuario){
+            timeState()
+            println("Estado del elevador $estado, piso $piso")
+            piso++
+        }
+        estado = EstadoElevador.DETENIDO
+        println("Estado del elevador $estado, en el piso $piso_usuario")
+        piso = piso_usuario
+    }
+
+    fun Bajar(piso_usuario: Int){
+
+    }
+    fun timeState(){
+        Thread.sleep(1000)
+    }
 }
--- app/src/main/java/com/example/ramonventura_examen/Main.kt
+++ app/src/main/java/com/example/ramonventura_examen/Main.kt
@@ -2,4 +2,15 @@ package com.example.ramonventura_examen
 
 fun main(){
     val elevador = Elevador(1,EstadoElevador.DETENIDO)
+    while(true){
+        print("Ingrese el piso al que desea ir: ")
+        val pisoUsuario: String? = readLine()
+        if (pisoUsuario != null) {
+            if(pisoUsuario.toInt()>elevador.piso){
+                elevador.Subir(pisoUsuario.toInt())
+            }else{
+
+            }
+        }
+    }
 }
